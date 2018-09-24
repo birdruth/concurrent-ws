d371a6dc18059e77e90ac916ddff484581d27266
jruth <bird_415@yahoo.com>
2018-09-24 10:17:44 -0400

    initial checkin

---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+var (
+	beats, port int
+	heartbeat   *time.Ticker
+	router      *gin.Engine
+)
+
+// Do all of this stuff first.
+func init() {
+	// In this example, we will hard code the port.  Later the environment
+	// will dictate.
+	port = 7718
+	// Set up the heartbeat ticker.
+	heartbeat = time.NewTicker(60 * time.Second)
+
+	// Setup the service router.
+	gin.SetMode(gin.ReleaseMode)
+	router = gin.New()
+
+	// These are the services we will be listening for.
+	router.GET("/beats", GetHeartbeatCount)
+	router.GET("/ping", PingTheAPI)
+} // func
+
+// GetHeartbeatCount sends the number of times the heartbeat ticker has
+// fired since the program started.
+func GetHeartbeatCount(c *gin.Context) {
+	content := gin.H{"payload": beats}
+	c.JSON(http.StatusOK, content)
+}
+
+// PingTheAPI lets the caller know we are alive.
+func PingTheAPI(c *gin.Context) {
+	content := gin.H{"payload": "PONG"}
+	c.JSON(http.StatusOK, content)
+}
+
+func main() {
+	// Dispatch a process into the background.
+	go func() {
+		// Now run it forever.
+		for {
+			// Wait for stuff to happen.
+			select {
+			// When the Heartbeat ticker is fired, execute this.
+			case <-heartbeat.C:
+				beats++
+				fmt.Printf("bump,Bump...\n")
+			} // select
+		} // for
+	}() // go func
+
+	// After the 'go func' is dispatched, start the server and listen on the
+	// specified port.
+	fmt.Printf("ready on port %d\n", port)
+	router.Run(":" + strconv.Itoa(port))
+}
